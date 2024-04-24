da2be0b9249de8a3fff26bc16259affe5caef24a
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-04-24 15:32:44 -0300

    added css to the modal add product

--- script/products.js
+++ script/products.js
@@ -5,16 +5,16 @@ let modalLink = document.getElementById('new_product');
 modalLink.addEventListener('click', function (event) {
     event.preventDefault();
     modalFactory(
-        '<h1>ADICIONAR PRODUTO</h1>\
-        <label>Nome</label>\
-        <input type="text" placeholder="Insira o nome do produto">\
-        <label>Descrição</label>\
-        <textarea></textarea>\
-        <label>Preço</label>\
-        <input type="number" placeholder="Insira o preço do produto">\
-        <label>Quantidade:</label>\
-        <input type="number" placeholder="Insira a quantidade em estoque">\
-        <button id="submit">ADICIONAR</button>',
+        '<h1 style="text-align: center;">ADICIONAR PRODUTO</h1>\
+        <label for="product_name_input">Nome</label></br>\
+        <input style="margin-top:10px;border-radius: 5px; border:solid 1px black; height: 100%;width: 100%;padding: 0.5rem;" type="text" placeholder="Insira o nome do produto" id="product_name_input"></br>\
+        <label  for="product_description_input" >Descrição</label></br>\
+        <textarea style="margin-top:10px;border-radius: 5px; border:solid 1px black; height: 100%;width: 100%;padding: 0.5rem;" id="product_description_input"></textarea></br>\
+        <label for="product_price_input">Preço</label></br>\
+        <input style="margin-top:10px;border-radius: 5px; border:solid 1px black; height: 100%;width: 100%;padding: 0.5rem;" id="product_price_input" type="number" placeholder="Insira o preço do produto"></br>\
+        <label for="product_quantity_input">Quantidade:</label></br>\
+        <input style="margin-top:10px;border-radius: 5px; border:solid 1px black; height: 100%;width: 100%;padding: 0.5rem;" id="product_quantity_input" type="number" placeholder="Insira a quantidade em estoque"></br>\
+        <button style="display: block; margin-top:2rem;margin-bottom:2rem;margin-left: auto; margin-right: auto; padding: 0.5rem 1rem; border: solid 1px black; border-radius: 5px; color: black; font-size: 1rem; cursor: pointer; transition: all 0.3s ease; font-weight: 600;" id="add_product">ADICIONAR</button>',
         {
             display: 'block',
             position: 'absolute',
@@ -27,7 +27,7 @@ modalLink.addEventListener('click', function (event) {
             borderRadius: "10px",
             backgroundColor: 'hsl(213 100% 98%)',
             padding: '0 2rem',
-            textAlign: 'center',
+            textAlign: 'left',
             boxShadow: '0 0 10px 0 hsl(213 100% 80%)',
             transition: 'all 0.3s ease-in-out',
         }
