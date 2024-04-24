94c91619376b917ac8312dd193dcdaefdd99af8e
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-04-24 17:31:49 -0300

    now loads the other page if login correctly

--- script/login-form.js
+++ script/login-form.js
@@ -6,7 +6,9 @@ let emailInput = document.getElementsByName("Email")[0]
 let loginButton = document.getElementsByName("loginButton")[0];
 loginButton.addEventListener('click', loginUser);
 
-function loginUser() {
+function loginUser(event) {
+    event.preventDefault(); // previne q o bgl seja enviado
+
     let email = emailInput.value;
     let password = senhaInput.value;
 
@@ -14,8 +16,8 @@ function loginUser() {
     let user = users.find(user => user.email === email && user.senha === password);
 
     if (user) {
-        //NEED TO DO A THING TO LOAD THE PAGE HERE
-        alert('Login efetuado com sucesso');
+        //CARREGA A OUTRA PGINA
+        window.location = "/html/products.html"
     } else {
         // NEED TO CREATE A MODAL TO SHOW THE USER NOT FOUND
         alert('User não encontrado');
